a62369f36aefffaa675bfbfb9d27c902876dff26
James Morris <blondfro@gmail.com>
2020-02-08 17:22:42 -0600

    created a homepage and added it to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import "bootstrap/dist/css/bootstrap-grid.min.css";
+import React from "react";
+import { render } from "react-dom";
+import HomePage from "./components/homepage";
+
+render(<HomePage/>, document.getElementById("root"));
