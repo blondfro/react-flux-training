37319f865a30208246439fea23065e0b2fe95802
James Morris <blondfro@gmail.com>
2020-02-10 17:32:24 -0600

    added in a redirect from a fake old link to understand  how it would work.

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ import AboutPage from "./AboutPage";
 import Header from "./common/header";
 import CoursesPage from "./CoursesPage";
 import NotFoundPage from "./NotFoundPage";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 
 function App() {
@@ -16,6 +16,7 @@ function App() {
                 <Route path="/" exact component={HomePage}/>
                 <Route path="/courses" component={CoursesPage}/>
                 <Route path="/about" component={AboutPage}/>
+                <Redirect from="/about-page" to="about"/>
                 <Route component={NotFoundPage}/>
             </Switch>
         </div>
