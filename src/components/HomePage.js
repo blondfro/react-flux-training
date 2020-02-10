51367eb6d77b7ba33153d623056df2e6bdf07cc2
James Morris <blondfro@gmail.com>
2020-02-10 17:03:13 -0600

    added a link to the home page using Link from react router.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 
 function HomePage() {
     return (
             <div className="jumbotron">
                 <h1>Pluralsight Administration</h1>
                 <p>React, Flux, and React Router for Ultra-responsibe web Apps</p>
+                <Link to="about" className="btn btn-primary">About</Link>
             </div>
         );
 }
