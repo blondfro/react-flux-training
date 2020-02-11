6815a3d3a629bd3b2dc7173f110e003894a0ad4a
James Morris <blondfro@gmail.com>
2020-02-10 18:46:26 -0600

    created a course form, added a button link to the course page to be able to add a course, added the course form to the course page and began passing needed props to the course form.

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import {Prompt} from "react-router-dom";
+import CourseForm from "./CourseForm";
 
 function ManageCoursePage(props) {
+    const [course, setCourse] = useState({
+        id: null,
+        slug: "",
+        title: "",
+        authorId: null,
+        category: ""
+    });
+
+    function handleTitleChange(event) {
+        const updatedCourse = {...course, title: event.target.value};
+        setCourse(updatedCourse);
+    }
+
     return (
         <>
             <h2>
                 Manage Course
             </h2>
+
             <Prompt
                 when={false}
                 message="Are you sure you want to leave this page?"
             />
-            {props.match.params.slug}
+            <CourseForm
+                course={course}
+                onTitleChange={handleTitleChange}
+            />
         </>
     )
 }
