b41faa64095218c75c9ba56914f23881a9bcfdfd
James Morris <blondfro@gmail.com>
2020-02-10 19:28:52 -0600

    updated the course form to make use of the input text component.

--- src/components/CourseForm.js
+++ src/components/CourseForm.js
@@ -5,53 +5,42 @@ function CourseForm(props) {
 
 
     return (
-    <form>
-        <div className="form-group">
-            <label htmlFor="title">Title</label>
-            <div className="field">
-                <input
-                    id="title"
-                    type="text"
-                    onChange={props.onChange}
-                    name="title"
-                    className="form-control"
-                    value={props.course.title }
-                />
-            </div>
-        </div>
+        <form>
+            <TextInput
+                id="title"
+                label="Title"
+                onChange={props.onChange}
+                name="title"
+                value={props.course.title}
+            />
 
-        <div className="form-group">
-            <label htmlFor="author">Author</label>
-            <div className="field">
-                <select
-                    id="author"
-                    name="authorId" onChange={props.onChange}
-                    value={props.course.authorId || ""} //if the value is null set it to an empty string
-                    className="form-control"
-                >
-                    <option value="" />
-                    <option value="1">Cory House</option>
-                    <option value="2">Scott Allen</option>
-                </select>
-            </div>
-        </div>
 
-        <div className="form-group">
-            <label htmlFor="category">Category</label>
-            <div className="field">
-                <input
-                    type="text"
-                    onChange={props.onChange}
-                    id="category"
-                    name="category"
-                    className="form-control"
-                    value={props.course.category}
-                />
+            <div className="form-group">
+                <label htmlFor="author">Author</label>
+                <div className="field">
+                    <select
+                        id="author"
+                        name="authorId" onChange={props.onChange}
+                        value={props.course.authorId || ""} //if the value is null set it to an empty string
+                        className="form-control"
+                    >
+                        <option value=""/>
+                        <option value="1">Cory House</option>
+                        <option value="2">Scott Allen</option>
+                    </select>
+                </div>
             </div>
-        </div>
 
-        <input type="submit" value="Save" className="btn btn-primary" />
-    </form>
+            <TextInput
+                id="category"
+                label="Category"
+                onChange={props.onChange}
+                name="category"
+                value={props.course.category}
+            />
+
+            <input type="submit" value="Save" className="btn btn-primary"/>
+        </form>
     )
 }
 
--- src/components/common/TextInput.js
+++ src/components/common/TextInput.js
@@ -10,7 +10,7 @@ function TextInput(props) {
 
     return (
         <div className={wrapperClass}>
-            <label htmlFor={props.id}>props.label</label>
+            <label htmlFor={props.id}>{props.label}</label>
             <div className="field">
                 <input
                     id={props.id}
