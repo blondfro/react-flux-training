d2dba1d971937c737b46450cf06896400c38dbdf
James Morris <blondfro@gmail.com>
2020-02-13 17:48:08 -0600

    setup edit courses to make use of the store.

--- src/stores/courseStore.js
+++ src/stores/courseStore.js
@@ -1,11 +1,11 @@
-import { EventEmitter } from "events";
+import {EventEmitter} from "events";
 import Dispatcher from "../appDispatcher";
 import actionTypes from "../actions/actionTypes";
 
 const CHANGE_EVENT = "change";
 let _courses = [];
 
-class CourseStore extends EventEmitter{
+class CourseStore extends EventEmitter {
     addChangeListener(callback) {
         this.on(CHANGE_EVENT, callback);
     }
@@ -29,18 +29,26 @@ class CourseStore extends EventEmitter{
 
 const store = new CourseStore();
 
-Dispatcher.register( action => {
+Dispatcher.register(action => {
     switch (action.actionType) {
         case actionTypes.CREATE_COURSE:
             _courses.push(action.course);
+            store.emitChange();
+            break;
+        case actionTypes.UPDATE_COURSE:
+            _courses = _courses.map( course =>
+                course.id === action.course.id
+                    ? action.course
+                    : course);
             store.emitChange();
             break;
         case actionTypes.LOAD_COURSES:
             _courses = action.courses;
             store.emitChange();
             break;
+
         default:
-            // nothing to do.
+        // nothing to do.
     }
 });
 
