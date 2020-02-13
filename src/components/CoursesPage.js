0dcc44b0c47de199dc164cec8886b8cdd8d0fb34
James Morris <blondfro@gmail.com>
2020-02-12 21:27:20 -0600

    setup load courses actions and setup the course page to receive the data on load from the store.

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,15 +1,27 @@
 import React, {useState, useEffect} from "react";
 import courseStore from "../stores/courseStore";
+import { loadCourses} from "../actions/courseActions";
+
 import CourseList from "./CourseList";
 import {Link} from "react-router-dom";
 
 
+
 function CoursesPage () {
-    const [courses, setCourses] = useState([]);
+    const [courses, setCourses] = useState(courseStore.getCourses());
 
     useEffect( () => {
-        setCourses(courseStore.getCourses());
+        courseStore.addChangeListener(onChange);
+        if (courseStore.getCourses().length === 0) {
+            loadCourses();
+        }
+        return () => {courseStore.removeChangeListener(onChange)} // cleanup on unmount.
     }, []);
+    
+    function onChange() {
+
+        setCourses(courseStore.getCourses());
+    }
 
     return (
         <>
